96cb954f0199cd3c3490c799029ef88d7538422d
Omar Alanazi <oalanazi@villacapital.sa>
2024-08-31 04:17:56 +0300

    initial axios setup

--- src/api/useAPI.ts
+++ src/api/useAPI.ts
@@ -1,8 +1,84 @@
-// TODO: add all api logic:
+import axios from "axios";
+import useAuthStore from "../stores/AuthStore";
+import useCustomToast, { ToastStatus } from "../hooks/useCustomToast";
 
-// ...
+const useAPI = () => {
+  const { showMessage } = useCustomToast();
+  const { accessToken, logout } = useAuthStore();
+  const BASE_URL = "https://localhost:7076";
 
-// TODO: add all types:
+  const api = axios.create({
+    baseURL: BASE_URL + "/api",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+  });
+
+  // axios middleware:
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      const { status } = error.response || {};
+
+      switch (status) {
+        case 400:
+        case 401:
+          break;
+        case 403:
+          showMessage("Forbidden. You don't have permission to access this resource.", ToastStatus.ERROR);
+          break;
+        case 404:
+          showMessage("Resource not found.", ToastStatus.ERROR);
+          break;
+        case 500:
+          showMessage("An unexpected error occurred. Please try again later.", ToastStatus.ERROR);
+          break;
+        case 503:
+          showMessage("Service unavailable. Please try again later.", ToastStatus.ERROR);
+          break;
+        default:
+          showMessage("An unexpected error occurred.", ToastStatus.ERROR);
+          break;
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  const submitLoginForm = async (request: LoginRequest): Promise<LoginResponse> => {
+    try {
+      const response = await api.post("/auth/login", request);
+      return response.data;
+    } catch (error: any) {
+      throw error;
+    }
+  };
+
+  return {
+    submitLoginForm,
+  };
+};
+
+export default useAPI;
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface UserClaims {
+  userId: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+}
 
 export interface LibraryDocument {
   id: number;
