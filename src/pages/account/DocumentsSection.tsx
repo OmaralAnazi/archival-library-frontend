927bf7ac6183bff2c23e53ac826d790a8c737878
Omar Alanazi <oalanazi@villacapital.sa>
2024-08-31 09:05:47 +0300

    profile enhancements

--- src/pages/account/DocumentsSection.tsx
+++ src/pages/account/DocumentsSection.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Table, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Heading, Table, Tbody, Th, Thead, Tr, Text, Center } from "@chakra-ui/react";
 import DocumentRow from "./DocumentRow";
 import { LibraryDocument } from "../../api/useAPI";
 
@@ -15,20 +15,28 @@ const DocumentsSection: React.FC<DocumentsSectionProps> = ({ documents, onView, 
         Your Documents
       </Heading>
       <Box maxH="400px" overflowY="auto" borderWidth="1px" borderRadius="md" borderColor="gray.200">
-        <Table variant="simple" size="md">
-          <Thead bg="gray.50">
-            <Tr>
-              <Th>Title</Th>
-              <Th>Date Created</Th>
-              <Th>Actions</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {documents.map((doc) => (
-              <DocumentRow key={doc.id} document={doc} onView={onView} onDelete={onDelete} />
-            ))}
-          </Tbody>
-        </Table>
+        {documents.length === 0 ? (
+          <Center py={10}>
+            <Text fontSize="lg" color="gray.600" textAlign="center">
+              You have no documents available.
+            </Text>
+          </Center>
+        ) : (
+          <Table variant="simple" size="md">
+            <Thead bg="gray.50">
+              <Tr>
+                <Th>Title</Th>
+                <Th>Date Created</Th>
+                <Th>Actions</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {documents.map((doc) => (
+                <DocumentRow key={doc.id} document={doc} onView={onView} onDelete={onDelete} />
+              ))}
+            </Tbody>
+          </Table>
+        )}
       </Box>
     </Box>
   );
